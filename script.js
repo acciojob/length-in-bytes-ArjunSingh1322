3d7d4b07c51066798654286712e76f92a5469be2
ArjunSingh1322 <as3388776@gmail.com>
2025-04-13 23:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+// const byteSize = (str) => {
+//   return new Blob([str]).size;
+// };
 const byteSize = (str) => {
-  // write your code here
+  return new TextEncoder().encode(str).length;
 };
+
+
 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
